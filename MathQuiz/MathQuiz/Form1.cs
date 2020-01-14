02f840bb164ad6fcd539709a57de273015a6cd98
Yvonne Chen <ywenchen1026@gmail.com>
2020-01-14 17:03:32 -0600

    move folder

---
+++ MathQuiz/MathQuiz/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathQuiz
+{
+    public partial class Form1 : Form
+    {
+        // Create a Random object called randomizer
+        // to generate random numbers.
+        Random randomizer = new Random();
+
+        // These integer variable store the numbers
+        // for the additional problem.
+        int addend1;
+        int addend2;
+
+        int timeLeft;
+
+        public void StartQuiz()
+        {
+
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+
+            sum.Value = 0;
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timeLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void plusRightLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartQuiz();
+            startButton.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Display the new time left by updating the timeLeft label.
+            if(timeLeft > 0)
+            {
+                timeLeft = timeLeft - 1;
+                timeLabel.Text = timeLeft + "seconds";
+            }
+            else
+            {
+                timer1.Stop();
+                timeLabel.Text = "Time's up!";
+                MessageBox.Show("You didn't finish in time.", "Sorry!");
+                startButton.Enabled = true;
+            }
+        }
+    }
+}
---
+++ MathQuiz/README.md
@@ -0,0 +1,28 @@
+# Tutorial 2: Create a Timed Math Quiz
+(Please refer to the Microsoft Docs website: https://docs.microsoft.com/en-us/visualstudio/ide/tutorial-2-create-a-timed-math-quiz?view=vs-2015)
+
+This is a step by step tutorial.
+
+- Step 1: Create a Project and Add Labels to Your Form
+
+Set the **AutoSize** property to True, which causes the button to automatically resize to fit the text.
+- Step 2: Create a Random Addition Problem
+
+The second statement sets the Enabled property of the startButton control to False so that the quiz taker can't choose the button during a quiz.
+- Step 3: Add a Countdown Timer
+
+Because the interval value is milliseconds, a value of 1000 causes the Tick event to fire every second.
+
+The code editor appears and displays the method for the Tick event handler that you just added.
+
+end here: Based on what you added
+- Step 4: Add the CheckTheAnswer() Method
+- Step 5: Add Enter Event Handlers for the NumericUpDown Controls
+- Step 6: Add a Subtraction Problem
+- Step 7: Add Multiplication and Division Problems
+- Step 8: Customize the Quiz
+
+
+
+
+
